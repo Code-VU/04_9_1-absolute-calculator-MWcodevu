5564931161bb28e6510ef66fa46d0faacff9179b
Matt Wyatt <matt.wyatt@veteransunited.com>
2023-02-20 14:18:36 -0600

    1

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,26 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    in_num  = int(input("Enter a number: "))
+    if in_num >= 21:
+        in_dif = (in_num -21)*2
+    else:
+        in_dif = 21 - in_num
+    print(in_dif)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
